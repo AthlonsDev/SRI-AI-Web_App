d0ceb3e0c7c8f724ed9591ee4ed1a587b3b70805
athlonsdev <athlons91@gmail.com>
2025-11-12 15:54:14 +0000

    Update transcription handling and display in frontend components; modify segment joining format for HTML compatibility

--- frontend/src/pages/model_1.jsx
+++ frontend/src/pages/model_1.jsx
@@ -1,14 +1,22 @@
 import React, { useCallback } from "react";
-import { Link, useParams } from "react-router-dom";
+import { data, Link, useParams } from "react-router-dom";
 import CardFile from "../components/CardFile";
 import CardForm from "../components/CardForm";
 import Sidebar from "../components/Sidebar";
 import { getSpeech } from "../api";
 import axios from 'axios'
 import { useEffect, useState } from "react";
+import { use } from "react";
+
+
 
 
 export default function Model_1(){
+    const [transcription, setTranscription] = useState("");
+// retireve transcription data from CardFile component
+    const handleSetTranscription = useCallback((data) => {
+        setTranscription(data);
+    }, []);
     return(
         <>
         {/* Sidebar Menu */}
@@ -19,8 +27,15 @@ export default function Model_1(){
 
             {/* Load model API */}
 
-            <CardFile/>
+            <CardFile text={data} setTranscription={handleSetTranscription}/>
 
+            <h2 class="text-center mt-4 mb-4">
+                Transcription
+            </h2>
+
+            <h8>
+                {transcription ? <div dangerouslySetInnerHTML={{ __html: transcription }} /> : "No Transcription Available"}
+            </h8>
         </>
     );
 };
