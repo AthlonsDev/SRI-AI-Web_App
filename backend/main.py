00a20e0d21f9b5b3b324c0c14131308b50c755c5
athlonsdev <athlons91@gmail.com>
2025-11-26 12:16:00 +0000

    Origin of Amplify link

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://main.d2m6wa3x69pn92.amplifyapp.com/'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
